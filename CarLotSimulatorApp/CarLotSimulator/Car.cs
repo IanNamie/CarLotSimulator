654451d2d7839f4be99ae8cc08f9a3a3f518818e
Ian Namie <Iannamie82@gmail.com>
2023-02-24 11:44:00 -0600

    finsihed exercise 2

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{ //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+  //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+    public class Car
+    {
+
+
+        public Car() { }
+        public Car(int year, string make, string model, string enginenoise, string honknoise, bool canDrive)
+        { 
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = enginenoise;
+            HonkNoise = honknoise;
+            IsDriveAble = canDrive;
+        }
+
+
+
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveAble { get; set; }
+
+        public void MakeEngineNoise(string engineNoise)
+        {
+            Console.WriteLine(engineNoise);
+        }
+        public void MakeHonkNoise(string honk)
+        {
+            Console.WriteLine(honk);
+        }
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -7,6 +7,7 @@ namespace CarLotSimulator
         static void Main(string[] args)
         {
             //TODO
+            var lot = new CarLot();
 
             //Create a seperate class file called Car
             //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
@@ -17,6 +18,60 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+            var honda = new Car();
+            honda.IsDriveAble = true;
+            honda.Year = 2017;
+            honda.Make = "Honda";
+            honda.Model = "Civic";
+            honda.EngineNoise = "vrrrrrrrr";
+            honda.HonkNoise = "meep meep";
+            lot.Cars.Add(honda);
+
+            var tesla = new Car() 
+            {
+                Make = "Tesla", 
+                Model = "Model S", 
+                Year = 2022, 
+                EngineNoise = "Silent", 
+                HonkNoise = "Beep Beep!!!", 
+                IsDriveAble = true
+            };
+            lot.Cars.Add(tesla);
+
+ var mazda = new Car(2019, "Mazda", "Cx 5", "MMMMmmmmmmmerrrrrrrrrrrrrRRRRRRRRRRRRRmrrrrrrrrrrr", "MEEP!! MEEP!!", true);
+            lot.Cars.Add(mazda);
+
+            foreach (var car in lot.Cars)
+            { 
+                Console.WriteLine($"Year: {car.Year}, Make:{car.Make}, Model: {car.Model}"); 
+            }
+            Console.WriteLine("----------------------------------");
+
+
+            Console.WriteLine(honda.Make);
+            /*Console.WriteLine(honda.Model);
+            Console.WriteLine(honda.Year);
+            Console.WriteLine($"{honda.IsDriveAble} the vehicle is driveable.");*/
+            honda.MakeEngineNoise(honda.EngineNoise);
+            honda.MakeHonkNoise(honda.HonkNoise);
+            Console.WriteLine("----------------------------------");
+
+            Console.WriteLine(tesla.Make);
+            /*Console.WriteLine(tesla.Model);
+            Console.WriteLine(tesla.Year);
+            Console.WriteLine($"{tesla.IsDriveAble} the vehicle is driveable.");*/
+            tesla.MakeEngineNoise(tesla.EngineNoise);
+            tesla.MakeHonkNoise(tesla.HonkNoise);
+            Console.WriteLine("----------------------------------");
+
+            Console.WriteLine(mazda.Make);
+            /*Console.WriteLine(mazda.Model);
+            Console.WriteLine(mazda.Year);
+            Console.WriteLine($"{mazda.IsDriveAble} the vehicle is driveable.");*/
+            mazda.MakeEngineNoise(mazda.EngineNoise);
+            mazda.MakeHonkNoise(mazda.HonkNoise);
+            Console.WriteLine("----------------------------------");
+
 
             //*************BONUS*************//
 
